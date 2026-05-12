468b10e5d61a8a0ddd35f3e16fb7d2e6584c4321
Janice1299 <janicelee20.25@ichat.sp.edu.sg>
2026-05-12 22:31:55 +0800

    Done stored procedure

--- models/reports.js
+++ models/reports.js
@@ -1,16 +1,28 @@
 const { query } = require('../database');
 
+// module.exports.generateModulesPerformance = function generateModulesPerformance() {
+//     const sql = 'SELECT * FROM get_modules_performance()';
+//     return query(sql)
+//         .then(function (result) {
+//             const rows = result.rows;
+//             return rows;
+//         })
+//         .catch(function (error) {
+//             throw error;
+//         });
+// };
+
 module.exports.generateModulesPerformance = function generateModulesPerformance() {
-    const sql = 'SELECT * FROM get_modules_performance()';
-    return query(sql)
-        .then(function (result) {
-            const rows = result.rows;
-            return rows;
-        })
-        .catch(function (error) {
-            throw error;
-        });
-};
+const sql = 'SELECT * FROM get_modules_performance() AS result';
+return query(sql)
+.then(function (result) {
+const rows = result.rows;
+return rows;
+})
+.catch(function (error) {
+throw error;
+});
+}
 
 module.exports.calculateStudentsGPA = function calculateStudentsGPA() {
     const sql = 'CALL calculate_students_gpa()';
